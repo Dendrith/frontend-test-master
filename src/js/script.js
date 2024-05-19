81f93610f509b4704a3a2e126184842cae94d7cd
rmaillo <rubenmv1983@gmail.com>
2024-05-19 21:48:43 +0200

    Commit_Domingo19

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Inserción de productos
 // Obtener la plantilla
 var source = document.getElementById("products-template").innerHTML;
   
@@ -15,3 +16,56 @@ fetch('products.json')
         document.getElementById("result").innerHTML = html;
     })
     .catch(error => console.error('Error:', error));
+
+// Animación filtro
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    eventListeners();
+    //eventListeners2();
+
+});
+
+// Función evento boton filtro
+function eventListeners() {
+    
+    const btnFilter = document.querySelector('.btn-filter');
+
+    btnFilter.addEventListener('click', openFilter);
+}
+
+// Función para agregar o eliminar clase mostrar
+function openFilter() {
+   
+    const filter = document.querySelector('.filter');
+
+    if (filter.classList.contains('mostrar')) {
+        filter.classList.remove('mostrar');
+    } else {
+        filter.classList.add('mostrar');
+    }
+
+}
+
+/*
+// Función para evento botón cerrar
+function eventListeners2() {
+
+    const btnClose = document.querySelector('.btn-close');
+
+    btnClose.addEventListener2('click', closeFilter);
+
+}
+
+
+// Función para agregar o eliminar clase mostrar
+function closeFilter() {
+    const filter2 = document.querySelector('.filter');
+
+    if (filter2.classList.contains('mostrar')) {
+        filter2.classList.remove('mostrar');
+    }else {
+        filter2.classList.add('mostrar');
+    }
+}
+*/
